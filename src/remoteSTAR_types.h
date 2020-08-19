a6715eb354822e7742ea9534c478a30f7273069e
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2020-08-19 03:24:12 -0500

    R/ and src/ are only files that are needed to build package

---
+++ src/remoteSTAR_types.h
@@ -0,0 +1,15 @@
+// [[Rcpp::depends(RcppEigen)]]
+#include <RcppEigen.h>
+
+using Eigen::LLT;
+using Eigen::Lower;
+using Eigen::Map;
+using Eigen::MatrixXd;
+using Eigen::MatrixXi;
+using Eigen::Upper;
+using Eigen::VectorXd;
+using Eigen::VectorXi;
+
+typedef Map<MatrixXd> MapMatd;
+typedef Map<MatrixXi> MapMati;
+typedef Map<VectorXd> MapVecd;
