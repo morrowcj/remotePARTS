46aff214a2525fb63131700bb91cd31daaa40189
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2020-11-16 17:10:53 -0600

    created failing test for fitGLS.R as reminder to write working tests

---
+++ inst/tinytest/test_fitGLS.R
@@ -0,0 +1,12 @@
+# Tests for fitGLS
+
+# coords = cbind(x = rnorm(10), y = rnorm(10))
+# D <- as.matrix(geosphere::distm(coords))
+# X <- matrix(rnorm(50), ncol = 5)
+# V <- cor(X)
+
+expect_true(FALSE, info = paste("Need to write working tests for fitGLS.R",
+                                "after writing the appropriate tests for",
+                                "fit_spatialcor() and fitV()")
+)
+
