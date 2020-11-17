1b4a0c5b394675d19a03d0ea21133cf0c89fab33
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2020-11-16 23:27:59 -0600

    fixed warnings from check

--- R/crosspart_worker.R
+++ R/crosspart_worker.R
@@ -1,5 +1,33 @@
 # crosspartition GLS worker
 
+#' Worker function 2 for partitioned GLS
+#'
+#' @details this is the second worker function for the partitioned GLS analysis.
+#'
+#' NOTE: currently, there is no parallel functionality and the partitioned
+#' form of the GLS is not implemented entirely in C++. Instead, the R function
+#' fitGLS.partition_rcpp() weaves between R and C++ on a single core. While
+#' this method is still much faster than the purely R implementation, migration
+#' to entirely C++ will greatly improve speed further. This migration requires
+#' calculating geographic distances with C++ which I've not yet written.
+#'
+#' Additionally, there seems to be a memory-related issue with this code. I've
+#' successfully used this function when partitions have 100 or fewer rows (too
+#' small). However, larger partitions cause a fatal error that causes a crash.
+#'
+#' @param xxi numeric matrix xx from  partition i
+#' @param xxj numeric matrix xx from  partition j
+#' @param xxi0 numeric matrix xx0 from  partition i
+#' @param xxj0 numeric matrix xx0 from  partition j
+#' @param invChol_i numeric matrix invcholV from  partition i
+#' @param invChol_j numeric matrix invcholV from  partition j
+#' @param Vsub numeric variance matrix for Xij (upper block)
+#' @param df1 first degree of freedom
+#' @param df2 second degree of freedom
+#'
+#' @examples #TBA
+#'
+#' @export
 crosspart_worker <- function(xxi, xxj, xxi0, xxj0, invChol_i, invChol_j, Vsub,
                              df1, df2){
   # coerce input to matrices
@@ -22,5 +50,5 @@ crosspart_worker <- function(xxi, xxj, xxi0, xxj0, invChol_i, invChol_j, Vsub,
   # stopifnot(all(check_posdef(V)))
 
   return(.Call(`_remoteSTAR_crosspart_worker_cpp`, xxi, xxj, xxi0, xxj0,
-               invChol_i, invChol_j, Vsub, df1, df2))
+               invChol_i, invChol_j, Vsub,  df1, df2))
 }
