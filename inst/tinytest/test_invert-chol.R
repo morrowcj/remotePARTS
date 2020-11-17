13abedefe3e8708b2b9b382b4b210e0361aec8c5
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2020-11-17 00:48:43 -0600

    removed test for unexported check_posdef()

--- inst/tinytest/test_invert-chol.R
+++ inst/tinytest/test_invert-chol.R
@@ -6,14 +6,14 @@ nug <- 0.25
 M2 <- (1 - nug) * M + nug * diag(nrow(M))
 
 ## test helper function check_posdef() ----
-expect_equivalent(check_posdef(matrix(1:9, 3)),
-                  c(TRUE, FALSE, FALSE),
-             info = "non positive definite input")
-expect_equivalent(check_posdef(matrix(c(1,4,7,4,5,8,7,8,9), 3)),
-                               c(TRUE,TRUE,FALSE),
-                  info = "square and symmetric input")
-expect_equivalent(check_posdef(M), c(TRUE, TRUE, TRUE),
-                  info = "positive definite input")
+# expect_equivalent(check_posdef(matrix(1:9, 3)),
+#                   c(TRUE, FALSE, FALSE),
+#              info = "non positive definite input")
+# expect_equivalent(check_posdef(matrix(c(1,4,7,4,5,8,7,8,9), 3)),
+#                                c(TRUE,TRUE,FALSE),
+#                   info = "square and symmetric input")
+# expect_equivalent(check_posdef(M), c(TRUE, TRUE, TRUE),
+#                   info = "positive definite input")
 
 ## correct calculation ----
 expect_equal(invert_chol(M),
