824dd238e0329bbddf26e1f558faac0a927c8b8e
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2020-12-04 13:24:05 -0600

    updated package doc to include function index

--- R/remoteSTAR-package.R
+++ R/remoteSTAR-package.R
@@ -1,8 +1,12 @@
+#' @section remoteSTAR functions:
+#' \code{\link{cls_star}}
+
 #' @keywords internal
 "_PACKAGE"
 
 #' @import Rcpp
 #' @importFrom Rcpp evalCpp
+#' @docType package
 ## usethis namespace: start
 #' @useDynLib remoteSTAR, .registration = TRUE
 ## usethis namespace: end
