da15471fdf2f729ddb2d33a0b15467d05d1a1074
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2021-01-14 12:22:49 -0600

    updated test file to use ndvi_AK instead of ndvi_AK3000

--- inst/tinytest/test_remoteSTAR-vs-TonyCode.R
+++ inst/tinytest/test_remoteSTAR-vs-TonyCode.R
@@ -7,15 +7,19 @@ load("data-for-tests.rda", verbose = TRUE)
 library(microbenchmark)
 
 # Test Data Setup ----
-load("../../data/ndvi_AK3000.rda", verbose = TRUE)
+load("../../data/ndvi_AK.rda", verbose = TRUE)
+ndvi_data <- ndvi_AK[!ndvi_AK$rare.land,]
+ndvi_data$land <- droplevels(ndvi_data$land)
+rm(ndvi_AK)
 
 
 N = 100
+boot.n = 1L
 
 set.seed(916)
 
-subsamp = sample.int(n = nrow(ndvi_AK3000), size = N)
-test.df = as.data.frame(ndvi_AK3000[subsamp, ])
+subsamp = sample.int(n = nrow(ndvi_data), size = N)
+test.df = as.data.frame(ndvi_data[subsamp, ])
 test.grid = test.df[, c("row", "col")]
 test.coord = test.df[, c("lng", "lat")]
 test.land = test.df[, c("land")]
@@ -32,7 +36,7 @@ test.form = "test.y ~ 0 + test.land"
 CLS.bench = microbenchmark(
   old.CLS = {old.CLS <-  CLS.fit(test.X, test.t)},
   new.CLS = {new.CLS <- cls_star(test.X, test.t)},
-  times = 10L
+  times = boot.n
 )
 ## compare output
 expect_equivalent(old.CLS[, c("site", "mean", "c", "t", "p", "b", "MSE")],
@@ -46,7 +50,7 @@ spatcor.bench = microbenchmark(
   old.r = {old.r <- spatialcor.fit(test.X, test.t, test.D, fit.n.sample = N)},
   new.r = {new.r <- fit_spatialcor(test.X, test.t, dist = D,
                                    location = test.coord, fit.n = N)},
-  times = 10L
+  times = boot.n
 )
 ## compare output
 expect_equivalent(old.r$spatialcor, new.r$spatialcor)
@@ -59,7 +63,7 @@ r = new.r$spatialcor
 ## benchmark
 V.bench = microbenchmark(old.V = {old.V <-  V.fit(test.D, r)},
                          new.V = {new.V <- fitV(test.D, r)},
-                         times = 10L)
+                         times = boot.n)
 ## compare output
 expect_equivalent(old.V, new.V)
 ## assign V
@@ -68,26 +72,27 @@ test.V = new.V
 
 # nugget fitting functions ----
 ## benchmark
-nug.bench = microbenchmark::microbenchmark(times = 10L,
+nug.bench = microbenchmark::microbenchmark(times = boot.n,
   old.nug = {old.nug <- nugget.fit(test.form, test.df, test.V)},
-  # new.nug_C = {new.nug_C <- optimize_nugget(X = model.matrix(~ 0 + test.land),
-  #                              V = test.V, y = test.y)},
+  new.nug_C = {new.nug_C <- optimize_nugget(X = model.matrix(~ 0 + test.land),
+                               V = test.V, y = test.y)},
   new.nug = {new.nug_R <- fitNugget(X = test.modmat,
                                     V = test.V, y = test.y)}
 ) # C++ version wont' run for large data... why?
 ## compare output
-expect_equivalent(old.nug, new.nug_R)
+expect_equivalent(old.nug, new.nug_R, new.nug_C)
 # expect_equivalent(old.nug, new.nug_C)
 
 # GLS functions ----
 ## benchmark
-GLS.bench = microbenchmark(times = 10L,
-                           old.GLS = {old.GLS <- GLS.fit(test.form, data = test.df,  V = test.V,
-                                              invcholV = invert_chol(test.V, new.nug_R))},
-                           new.GLS = {new.GLS <- fitGLS(X = test.modmat,
-                                             y = test.y, V = test.V, nugget = new.nug_R,
-                                             X0 = cbind(rep(1, nrow(test.X))), save_xx = TRUE,
-                                             threads = 1)})
+GLS.bench = microbenchmark(
+  old.GLS = {old.GLS <- GLS.fit(test.form, data = test.df,  V = test.V,
+                                invcholV = invert_chol(test.V, new.nug_R))},
+  new.GLS = {new.GLS <- fitGLS(X = test.modmat,
+                               y = test.y, V = test.V, nugget = new.nug_R,
+                               X0 = cbind(rep(1, nrow(test.X))), save_xx = TRUE,
+                               threads = 1)},
+  times = boot.n)
 ## compare output
 expect_equivalent(old.GLS$coef, new.GLS$betahat)
 expect_equivalent(old.GLS$t, new.GLS$tstat)
@@ -101,6 +106,6 @@ if(FALSE){
   library(ggplot2)
   autoplot(CLS.bench)
   autoplot(V.bench)
-  autoplot(nug.bench) # new is faster
-  autoplot(GLS.bench) # new is marginally slower (N = 100, trheads = 4)
+  autoplot(nug.bench) # C++ version is about 2x faster (only when compiled) N = 1000
+  autoplot(GLS.bench) # C++ version is slightly slower (when compiled) N = 1000
 }
