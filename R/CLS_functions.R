ff5702d9b97aca9ea61a3b7b9a183447997dbeff
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2021-01-16 00:27:08 -0600

    mod. CLS functs, deprec. cls_star() for fitCLS.map(), made S3 meths

--- R/CLS_functions.R
+++ R/CLS_functions.R
@@ -3,125 +3,193 @@
 #'
 #' @description see `?cls_star()`
 #'
-#' @param x numeric vector of length p containing remote sensing data
-#' @param t numeric vector of length p containing the values for time. Recommended:
-#' `scale(1:ncol(X))`
-#' @param Z (optional) a numeric vector/matrix with p columns and q rows. Each
-#' row contains a different covariate measured at the same site as x at the same
-#' p time points.
+#' @param x numeric vector of length p containing time series data for one
+#' location (i.e. a pixel)
+#' @param t numeric vector of length p containing the values for time.
+#' @param Z (optional) a numeric vector/matrix with p rows and q columns. Each
+#' column contains a different covariate measured at the same site as x at the
+#' same p time points.
+#' @param save_AR.df should the auto-regression data frame be returned?
+#' default: FALSE
 #'
-#' @return a list of 3 or 4 elements:
-#' `coef` the model coefficients from `summary(lm(...))$coef` where `x_t0` is the
-#' effect of a pixel's value at the previous time on its value  at the current
-#' time point and `time` is the effect of time.
-#' `MSE` means squared error of the regression model
-#' `resids` the residual errors of the model
-#' `Z` the coefficients for optional covariates
+#' @return list of 3 elements: \code{$call} the function call used to produce
+#' the output, \code{$fm} the model object fit using \code{stats::lm()}, and, if
+#' if \code{save_AR.df = TRUE}, \code{$AR.df} an AR data frame built with
+#' \code{AR_df()} .
+#'
+#' @seealso [AR_df()] for AR data frames and [fitCLS.map()] for fitting full-map
+#' time series
 #'
 #' @export
 #'
-#' @examples #TBA
-fitCLS <- function(x, t, Z = NULL) {
-  # variables
-  t_n = length(t)
-
-  # covar handling
-  if (is.null(Z) | missing(Z)) {
-    Zs = NA
-  } else {
-    Zs = as.matrix(Z)
-    stopifnot(dim(Z)[1] == t_n)
-    Zs = Zs[2:t_n, ]
-  }
-
-  # data frame for the model
-  tmp <- data.frame(
-    x_t = x[2:t_n], #X_{t}
-    x_t0 = x[1:(t_n - 1)], #X_{t-1}
-    time = t[2:t_n], #T_{t}
-    Z = Zs)
-
+#' @examples
+#' data(ndvi_AK3000)
+#' x = unlist(ndvi_AK3000[1, -c(1:6)]) # time series for first pixel only
+#' t = 1:length(x) # time points
+#' ## CLS without covariates
+#' fitCLS(x = x, t = t)
+#'
+#' ## now with 2 covariates
+#' Z = cbind(rnorm(length(x)), rnorm(length(x)))
+#' fitCLS(x = x, t = t, Z = Z)
+#'
+#' ## get the AR data frame with save_AR.df = TRUE
+#' fitCLS(x = x, t = t, save_AR.df = TRUE)$AR.df
+#'
+#' ## print a summary
+#' summary(fitCLS(x = x, t = t))
+#'
+#' ## use get_fm() to extract $fm, which is a stats::lm() object
+#' fm = get_fm(fitCLS(x = x, t = t))
+#' ## then treat it just like lm()
+#' summary(fm)
+#' resid(fm)
+fitCLS <- function(x, t, Z = NULL, save_AR.df = FALSE) {
+  stopifnot(length(x) == length(t)) # check
+  # build AR data frame
+  AR.df <- AR_df(x, t, Z)
   # fit the model
-  if(is.null(Z) | missing(Z)){
-    fm <- lm(x_t ~ x_t0 + time, data = tmp)
+  fm <- if(is.null(Z)) {
+     lm(x.tj ~ x.ti + t.j, data = AR.df)
   } else {
-    fm <- lm(x_t ~ x_t0 + time + Z, data = tmp)
+     lm(x.tj ~ x.ti + t.j + z, data = AR.df)
   }
-
-  out <- list(coef = summary(fm)$coef, MSE = summary(fm)$sigma^2,
-              resids = resid(fm))
-
-  # add Z to output
-  if (!is.null(Z) & !missing(Z)){
-    out$Z <- summary(fm)$coef[-c(1:3), ]
-  } else {out$Z <-  NULL}
+  # build output list
+  out <- list(call = match.call(),
+              fm = fm)
+  # add AR.df to output list
+  if(save_AR.df){
+    out$AR.df = AR.df
+  } else {out$AR.df = NULL}
+  # set remoteCLS class
+  class(out) <- c("remoteCLS", "pixel")
 
   return(out)
 }
 
-# cls_star ----
-#' CLS regression of remote sensing data
+## Function to replace cls_star ----
+#' Title
 #'
-#' @description
-#' Fit a constrained linear regression model to remote sensing data `X`, testing
-#' the effects of time `t`, `x_{i, t-1}`, and, optionally `z_{i, t-1}` on
-#' `x_{i, t}`.
+#' @param X
+#' @param t
+#' @param ret_xi.coef
+#' @param ret_int.coef
+#' @param ret_MSE
+#' @param ret_resid
 #'
-#' @details
-#' This function is a wrapper that calls the single-site function `fitCLS()`
-#' for each site. At present, matrices `X` and `Z` must not contain any `NA`s.
-#'
-#' @param X n x p numeric matrix (usually of of remote sensing observations)
-#'  taken from n sites (rows) and p time points (columns).
-#' @param t numeric vector of length p containing the values for time. Recommended:
-#' `scale(1:ncol(X))`.
-#' @param Z.list Optional list with q elements. Each element of this list
-#' should be an n x p numeric matrix (`z_{i, t}`) with values corresponding to a
-#' covariate measured at each site:time combination.
-#'
-#' @return an n x 8 matrix with the following columns:
-#' `site` the site number from 1-n
-#' `mean` the site average of `x_{i}` (i.e. `rowMeans(X)`)
-#' `Est` the coefficient estimate for the effect of time
-#' `SE` standard error of the coefficient estimate
-#' `t` student's t-test statistic for the effect of time
-#' `p` p-value of the two-tailed t test for the effect of time
-#' `MSE` site-specific mean squared error
-#' `x_t0.EST` the coefficient estimate for the effect of x_{i, t-1} on x_{i, t}
-#'
+#' @return
 #' @export
 #'
-#' @examples #TBA
-#' # starting parameters
-#' n = 25; p = 20; beta = .2
-#'
-#' # simulate data
-#' time = scale(1:p)
-#' X = matrix(rnorm(n*p), ncol = p) # X without time effect
-#' # effect of time from 1 - p
-#' betaT = matrix(rep(time, each = n) * beta, ncol = p)
-#'
-#' # fit the model
-#' cls_star(X*betaT, time)
+#' @examples
+fitCLS.map <- function(X, t, ret_xi.coef = FALSE, ret_int.coef = FALSE,
+                       ret_MSE = TRUE, ret_resid = TRUE){
+  n.pixels = nrow(X)
+  n.time = length(t)
+  stopifnot(ncol(X) == n.time)
 
-cls_star <- function(X, t, Z.list = NULL){
-  # data to fill
-  tmp <- data.frame(site = 1:nrow(X), mean = rowMeans(X),
-                  Est = NA, SE = NA, t = NA, p = NA, MSE = NA,
-                  x_t0.EST = NA)
+  ## run CLS
+  cls.list <- lapply(1:n.pixels, function(x){
+    fitCLS(X[x,], t, save_AR.df = TRUE)
+  })
 
-  for (i in 1:nrow(X)) {
-    if (is.null(Z.list) | missing(Z.list)) {
-      fit <- fitCLS(X[i, ], t, Z = NULL)
-    } else {
-      stopifnot(sapply(Z.list, function(Y)dim(Y)==dim(X)))
-      z <-  as.matrix(sapply(Z.list, function(Y){Y[i, ]}))
-      fit <- fitCLS(X[i, ], t, z)
-    }
-    # d[i, "mean"] <- mean(X[i, ])
-    tmp[i, 3:6] <-  fit$coef[3, ]
-    tmp[i, "MSE"] <- fit$MSE
-    tmp[i, "x_t0.EST"] <- fit$coef[2, 1]
+  ## get coefficients from cls
+  coef.list <- lapply(cls.list, function(x){
+    as.data.frame(summary(get_fm(x))$coefficient)
+  })
+
+  ## setup coefficient tables
+  out.list <- list(call = match.call(),
+                   time.coef = as.data.frame(matrix(NA, ncol = 4, nrow = n.pixels)))
+  names(out.list$time.coef) <- c("Est", "SE", "t","p.t")
+  out.list$mean = rowMeans(X)
+  if (ret_xi.coef){
+    out.list$xi.coef <- as.data.frame(matrix(NA, ncol = 4, nrow = n.pixels))
+    names(out.list$xi.coef) <- c("Est", "SE", "t","p.t")
   }
-  return(tmp)
+  if (ret_int.coef){
+    out.list$int.coef <- as.data.frame(matrix(NA, ncol = 4, nrow = n.pixels))
+    names(out.list$int.coef) <- c("Est", "SE", "t","p.t")
+  }
+  if (ret_MSE) {out.list$MSE = vector("numeric", n.pixels)}
+  if (ret_resid) {out.list$residuals = matrix(NA, ncol = n.time - 1, nrow = n.pixels)}
+
+  # build the tables
+  for (i in 1:n.pixels){
+    out.list$time.coef[i, ] <- coef.list[[i]]["t.j", ]
+    if (ret_int.coef) {out.list$int.coef[i, ] <- coef.list[[i]]["(Intercept)", ]}
+    if (ret_xi.coef) {out.list$xi.coef[i, ] <- coef.list[[i]]["x.ti", ]}
+    if (ret_MSE) {out.list$MSE[i] <- sigma(get_fm(cls.list[[i]]))^2}
+    if (ret_resid) {out.list$residuals[i, ] <- residuals(get_fm(cls.list[[i]]))}
+  }
+
+
+  class(out.list) <- c("remoteCLS", "map")
+
+  return(out.list)
 }
+
+# # cls_star DEPRICATED ----
+# #' CLS regression of remote sensing data
+# #'
+# #' @description
+# #' Fit a constrained linear regression model to remote sensing data `X`, testing
+# #' the effects of time `t`, `x_{i, t-1}`, and, optionally `z_{i, t-1}` on
+# #' `x_{i, t}`.
+# #'
+# #' @details
+# #' This function is a wrapper that calls the single-site function `fitCLS()`
+# #' for each site. At present, matrices `X` and `Z` must not contain any `NA`s.
+# #'
+# #' @param X n x p numeric matrix (usually of of remote sensing observations)
+# #'  taken from n sites (rows) and p time points (columns).
+# #' @param t numeric vector of length p containing the values for time. Recommended:
+# #' `scale(1:ncol(X))`.
+# #' @param Z.list Optional list with q elements. Each element of this list
+# #' should be an n x p numeric matrix (`z_{i, t}`) with values corresponding to a
+# #' covariate measured at each site:time combination.
+# #'
+# #' @return an n x 8 matrix with the following columns:
+# #' `site` the site number from 1-n
+# #' `mean` the site average of `x_{i}` (i.e. `rowMeans(X)`)
+# #' `Est` the coefficient estimate for the effect of time
+# #' `SE` standard error of the coefficient estimate
+# #' `t` student's t-test statistic for the effect of time
+# #' `p` p-value of the two-tailed t test for the effect of time
+# #' `MSE` site-specific mean squared error
+# #' `x_t0.EST` the coefficient estimate for the effect of x_{i, t-1} on x_{i, t}
+# #'
+# #' @export
+# #'
+# #' @examples #TBA
+# #' # starting parameters
+# #' n = 25; p = 20; beta = .2
+# #'
+# #' # simulate data
+# #' time = scale(1:p)
+# #' X = matrix(rnorm(n*p), ncol = p) # X without time effect
+# #' # effect of time from 1 - p
+# #' betaT = matrix(rep(time, each = n) * beta, ncol = p)
+# #'
+# #' # fit the model
+# #' cls_star(X*betaT, time)
+# cls_star <- function(X, t, Z.list = NULL){
+#   # data to fill
+#   tmp <- data.frame(pixel = 1:nrow(X), mean = rowMeans(X),
+#                     Est = NA, SE = NA, t = NA, p = NA, MSE = NA,
+#                     x_t0.EST = NA)
+#
+#   for (i in 1:nrow(X)) {
+#     if (is.null(Z.list) | missing(Z.list)) {
+#       fit <- fitCLS(X[i, ], t, Z = NULL)
+#     } else {
+#       stopifnot(sapply(Z.list, function(Y)dim(Y)==dim(X)))
+#       z <-  as.matrix(sapply(Z.list, function(Y){Y[i, ]}))
+#       fit <- fitCLS(X[i, ], t, z)
+#     }
+#     # d[i, "mean"] <- mean(X[i, ])
+#     tmp[i, 3:6] <-  fit$coef[3, ]
+#     tmp[i, "MSE"] <- fit$MSE
+#     tmp[i, "x_t0.EST"] <- fit$coef[2, 1]
+#   }
+#   return(tmp)
+# }
--- R/S3-objects-and-methods.R
+++ R/S3-objects-and-methods.R
@@ -1,5 +1,175 @@
 ## remotePARTS objects (S3) and methods
 
+#' generic method to extract fitted model from a complex object
+#'
+#' @param object complex S3 object
+#' @param ... additional arguments passed to methods
+#'
+#' @seealso [fitCLS()] for example usage
+#'
+#' @export
+get_fm = function(object, ...){ ## get the model object from complex objects
+  UseMethod("get_fm")
+}
+
+# CLS ----
+
+#' Build AR data frame
+#'
+#' @param x length p time series vector
+#' @param t length p vector of time
+#' @param Z optional vector or design matrix of covariates. if only one
+#' covariate, \code{length(Z)} = p. If more than one, \code{nrow(Z)} = p
+#'
+#' @return
+#' @export
+#'
+#' @examples
+AR_df <- function(x, t, Z = NULL){
+  # variables
+  t_n = length(t)
+
+  # covar handling
+  if (is.null(Z)) {
+    Zs = NA
+  } else {
+    Zs = as.matrix(Z)
+    stopifnot(dim(Z)[1] == t_n)
+    Zs = Zs[2:t_n, ]
+  }
+
+  # data frame for the model
+  AR.df <- data.frame(
+    x.tj = x[2:t_n], #X_{t}
+    x.ti = x[1:(t_n - 1)], #X_{t-1}
+    t.j = t[2:t_n] #T_{t}
+  )
+
+  # fit the model
+  if (!is.null(Z)) AR.df$z = Zs
+
+  return(AR.df)
+}
+
+#' get linear model object from remote CLS
+#'
+#' @param obj remoteCLS object
+#'
+#' @return lm class object from stats::lm
+#' @export
+get_fm.remoteCLS <- function(obj){
+  stopifnot("remoteCLS" %in% class(obj))
+  if ("pixel" %in% class(obj)) {
+    return(obj$fm)
+  } else if ("map" %in% class(obj)) {
+
+  } else
+    NULL
+}
+
+#' print remoteCLS object
+#'
+#' @param obj remoteCLS object
+#' @export
+print.remoteCLS <- function(obj){
+  stopifnot("remoteCLS" %in% class(obj))
+
+  ## Pixel version
+  if ("pixel" %in% class(obj)) {
+    # call
+    print(obj$call)
+    cat("\n")
+    # then lm
+    cat("linear model: ")
+    print(obj$fm$call$formula)
+    print(obj$fm$coefficients)
+
+  } else if ("map" %in% class(obj)) {
+    ## Map version
+    print(obj$call)
+    cat("\nEffect of time:\n")
+    print(obj$time.coef)
+  }
+}
+
+#' return remoteCLS obj as data frame
+#'
+#' @param obj remoteCLS map object (see \code{fitCLS.map()})
+#'
+#' @return as.data.frame(obj$time.coef)
+#' @export
+as.data.frame.remoteCLS <- function(obj){
+  stopifnot("remoteCLS" %in% class(obj))
+
+  if ("pixel" %in% class(obj)) {
+   stop("no applicable method for as.data.frame()")
+  } else if ("map" %in% class(obj)) {
+    return(as.data.frame(obj$time.coef))
+  }
+}
+
+#' summary of remoteCLS object
+#'
+#' @param obj remoteCLS object
+#'
+#' @export
+summary.remoteCLS <- function(obj){
+  stopifnot("remoteCLS" %in% class(obj))
+
+  ## Pixel version
+  if ("pixel" %in% class(obj)) {
+    fm <- get_fm(obj)
+    smry <- summary(fm)
+    MSE = sigma(fm)^2
+
+    # coefficient table
+    cat("coefficients:\n")
+    print(format(as.data.frame(smry$coefficients),
+           digits = 2, nsmall = 2, scientific = 1))
+    # MSE
+    cat("\nMSE =", format(MSE, digits = 3))
+
+
+  } else if ("map" %in% class(obj)) {
+    ## Map version
+    cat("Summary\n")
+    cat("\neffect of time:\n")
+    print(summary(obj$time.coef))
+    if(!is.null(obj$xi.coef)){
+      cat("\neffect of previous x:\n")
+      print(summary(obj$xi.coef))
+    }
+    if(!is.null(obj$int.coef)){
+      cat("\nintercept:\n")
+      print(summary(obj$int.coef))
+    }
+    if(!is.null(obj$MSE)){
+      cat("\nMSE:\n")
+      print(summary((obj$MSE)))
+    }
+  }
+}
+
+#' resisduals of remoteCLS object
+#'
+#' @param obj remoteCLS object
+#'
+#' @return model residuals
+#' @export
+residuals.remoteCLS <- function(obj){
+  stopifnot("remoteCLS" %in% class(obj))
+  ## Pixel version
+  if ("pixel" %in% class(obj)) {
+    fm = get_fm(obj)
+    return(fm$residuals)
+  } else if ("map" %in% class(obj)) {
+    obj$residuals
+  }
+}
+
+
+# GLS ----
+
 #' remoteGLS constructor (S3)
 #'
 #' @return an empty S3 object of class "remoteGLS"
@@ -46,6 +216,7 @@ remoteGLS <- function(form){
 #' @return formatted output for remoteGLS object
 #' @export
 print.remoteGLS <- function(obj, print.call = TRUE){
+  stopifnot("remoteGLS" %in% class(obj))
 # print.remoteGLS <- function(obj){
   if(is.null(unlist(obj[-1]))){
     cat("empty remoteGLS object\ncall: ")
@@ -80,7 +251,10 @@ print.remoteGLS <- function(obj, print.call = TRUE){
 }
 
 ## summary function
-# summary.remoteGLS <- function(obj){}
+# summary.remoteGLS <- function(obj){
+
+
+# Other ----
 
 ## Test a small C++ function to alter list elements ----
 if (FALSE) {
@@ -117,6 +291,8 @@ if (FALSE) {
   .test_remoteGLS(tmp) # changes the values within tmp
   print(tmp)
 }
+
+
 
 ## ----
 
