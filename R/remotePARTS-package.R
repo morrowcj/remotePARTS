1494628c69fb9b2468463985c67c375fc4830922
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2021-02-15 14:28:57 -0600

    imported functions from stats package

--- R/remotePARTS-package.R
+++ R/remotePARTS-package.R
@@ -5,6 +5,8 @@
 "_PACKAGE"
 
 #' @importFrom Rcpp evalCpp
+#' @importFrom stats aggregate coef cor formula lm logLik model.matrix
+#' model.response nls nls.control optimize p.adjust pf pt residuals rnorm sigma
 #' @docType package
 ## usethis namespace: start
 #' @useDynLib remotePARTS, .registration = TRUE
