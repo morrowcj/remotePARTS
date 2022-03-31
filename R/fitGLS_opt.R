ef4afd68e67761d7ef5bce1c1116a52de87868f9
Clay Morrow <25436787+morrowcj@users.noreply.github.com>
2022-03-31 16:30:56 -0500

    reverted fitGLS_opt to older version

--- R/fitGLS_opt.R
+++ R/fitGLS_opt.R
@@ -19,11 +19,6 @@
 #' a final GLS will not be fit with the estimated parameters
 #' @param formula0,save.xx,save.invchol,no.F arguments passed to \code{fitGLS}
 #' for final GLS output
-#' @param trans optional list of functions for transforming the values in
-#' \code{start} or \code{fixed} in order to constrain the parameter space within
-#' \code{optim}
-#' @param backtrans optional list of functions for back-transforming parameters
-#' to their correct scale (for use with \code{trans})
 #' @param ... additional arguments passed to \code{stats::optim()}
 #'
 #' @details \code{fitGLS_opt} fits a GLS by estimating spatial parameters from
@@ -64,22 +59,6 @@
 #' This process is very computationally intensive and may take a long time to
 #' finish depending upon your machine and the size of the data.
 #'
-#' Sometimes \code{optim} can have a difficult time finding a reasonable solution
-#' and without any constraits on parameter space (with certain algorithms), results
-#' may even be nonsensical. To combat this, \code{fitGLS_opt} has the arguments
-#' \code{trans} and \code{backtrans} which allow you to transform
-#' (and back-transform) parameters to a different scale. For example, you may
-#' want to force the 'range' parameter between 0 and 1. The logit function can
-#' do just that, as its limits are -Inf and Inf as x approaches 0 and 1,
-#' respectively. So, we can set \code{trans} to the logit function:
-#' \code{trans = list(range = function(x)log(x/(1-x)))}. Then we need to set
-#' \code{backtrans} to the inverse logit function to return a parameter value
-#' between 0 and 1: \code{backtrans = list(range = function(x)1/(1+exp(-x)))}.
-#' This will force the optimizer to only search for the range parameter in the
-#' space from 0 to 1. Any other constraint function can be used for \code{trans}
-#' provided that there is a matching back-transformation.
-#'
-#'
 #' @seealso \code{\link{fitCor}} for estimating spatial parameters from time
 #' series residuals; \code{\link{fitGLS}} for fitting GLS and with the option
 #' of estimating the maximum-likelihood nugget component only.
@@ -110,16 +89,6 @@
 #'              coords = df[, c("lng", "lat")],
 #'              start = c(range = .1), fixed = c("nugget" = 0),
 #'              method = "Brent", lower = 0, upper = 1)
-#'
-#' ## constrain nugget to 0 and 1
-#' logit <- function(p) {log(p / (1 - p))}
-#' inv_logit <- function(l) {1 / (1 + exp(-l))}
-#'
-#' fitGLS_opt(formula = CLS_coef ~ 0 + land, data = df,
-#'            coords = df[, c("lng", "lat")],
-#'            start = c(range = .1, nugget = 1e-10),
-#'            trans = list(nugget = logit), backtrans = list(nugget = inv_logit),
-#'            opt.only = TRUE)
 #' }
 #' @export
 fitGLS_opt <- function(formula, data = NULL, coords, distm_FUN = "distm_scaled",
@@ -128,33 +97,15 @@ fitGLS_opt <- function(formula, data = NULL, coords, distm_FUN = "distm_scaled",
                        fixed = c(), opt.only = FALSE,
                        formula0 = NULL, save.xx = FALSE, save.invchol = FALSE,
                        no.F = TRUE,
-                       trans = list(), backtrans = list(),
                        ...){
   call = match.call()
 
-  # transform variables to constrain them in optim, if needed
-  is.trans = FALSE
-  if(length(trans) > 0 & length(backtrans) > 0){
-    stopifnot(length(trans) == length(backtrans))
-    stopifnot(names(trans) == names(backtrans))
-    stopifnot(all(names(trans) %in% c(names(start), names(fixed))))
-    is.trans = TRUE
-    for(n in names(trans)){
-      if(n %in% names(start)){
-        start[n] = do.call(trans[[n]], list(start[n]))
-      }
-      if(n %in% names(fixed)){
-        fixed[n] = do.call(trans[[n]], list(fixed[n]))
-      }
-    }
-  }
 
   # create a list of arguments to pass to do.call
   arg.list <- list(par = start[! names(start) %in% names(fixed)], #parameters to optimze
                    fn = fitGLS_opt_FUN, formula = formula, data = data,
                    coords = coords, covar_FUN = covar_FUN,
-                   distm_FUN = distm_FUN,
-                   is.trans = is.trans, backtrans = backtrans
+                   distm_FUN = distm_FUN
   )
 
   # append fixed parameters, if they exist
@@ -167,21 +118,6 @@ fitGLS_opt <- function(formula, data = NULL, coords, distm_FUN = "distm_scaled",
 
   # call optim, and pass arguments
   opt.out <- do.call(optim, args = arg.list)
-
-  # back-transform the parameter values to their original scale
-  if(is.trans){
-    for(n in names(backtrans)){
-      if(n %in% names(opt.out$par)){
-        opt.out$par[n] = do.call(backtrans[[n]], list(opt.out$par[n]))
-      }
-      if(n %in% names(start)){
-        start[n] = do.call(backtrans[[n]], list(start[n]))
-      }
-      if(n %in% names(fixed)){
-        fixed[n] = do.call(backtrans[[n]], list(fixed[n]))
-      }
-    }
-  }
 
   if(opt.only){
     return(opt.out)
@@ -210,11 +146,6 @@ fitGLS_opt <- function(formula, data = NULL, coords, distm_FUN = "distm_scaled",
 #' @param distm_FUN a distm function
 #' @param formula GLS model formula
 #' @param data data source
-#' @param is.trans logical: are any of the values in \code{op} or \code{fp}
-#' transformed, needing back-transformation?
-#' @param backtrans optional: a named list of functions used to backtransform any element
-#' of \code{op} or \code{fp}. Names must correspond to names in \code{op}
-#' or \code{fp}.
 #'
 #' @return \code{fitGLS_opt_FUN} returns the negative log likelihood of a GLS,
 #' given the parameters in \code{op} and \code{fp}
@@ -230,35 +161,10 @@ fitGLS_opt <- function(formula, data = NULL, coords, distm_FUN = "distm_scaled",
 #' remotePARTS:::fitGLS_opt_FUN(op = c(range = .1), fp = c(nugget = 0),
 #'                              formula = CLS_coef ~ 0 + land, data = df,
 #'                              coords = coords)
-#'
-#' logit <- function(p) {log(p / (1 - p))}
-#' inv_logit <- function(l) {1 / (1 + exp(-l))}
-#'
-#' # input logit-transformed range parameters
-#' remotePARTS:::fitGLS_opt_FUN(op = c(range = .1, nugget = logit(.2)),
-#'                              formula = CLS_coef ~ 0 + land, data = df,
-#'                              coords = coords, is.trans = TRUE,
-#'                              backtrans = list(nugget = inv_logit))
-#' # transformed range and nugget
-#' remotePARTS:::fitGLS_opt_FUN(op = c(range = logit(.1), nugget = logit(.2)),
-#'                              formula = CLS_coef ~ 0 + land, data = df,
-#'                              coords = coords, is.trans = TRUE,
-#'                              backtrans = list(nugget = inv_logit, range = inv_logit))
 #' }
-fitGLS_opt_FUN <- function(op, fp, formula, data = NULL, coords, covar_FUN = "covar_exp", distm_FUN = "distm_scaled",
-                           is.trans = FALSE, backtrans = list()){
+fitGLS_opt_FUN <- function(op, fp, formula, data = NULL, coords, covar_FUN = "covar_exp", distm_FUN = "distm_scaled"){
   ## combine the optimized and fixed parameters into one vector
   all.pars <- if(missing(fp)){op}else{c(op, fp)}
-
-  if(is.trans == TRUE) {
-    if (is.null(names(backtrans)) | !all(names(backtrans) %in% names(all.pars))) {
-      stop("backtrans mut be a list with element names matching names in op or fp")
-    }
-    for (n in names(backtrans)){
-      all.pars[n] = do.call(backtrans[[n]], list(all.pars[n]))
-    }
-  }
-
   ## extract the nugget
   nug = ifelse("nugget" %in% names(all.pars), all.pars["nugget"], 0)
   ## extract the non-nugget parameters
@@ -270,9 +176,9 @@ fitGLS_opt_FUN <- function(op, fp, formula, data = NULL, coords, covar_FUN = "co
   args = append(list(d = V), as.list(sp.pars))
   V = do.call(cov.f, args) # replace with covariance
   ## Calculate log-likelihood
-  logLik = suppressWarnings(fitGLS(formula = formula, data = data, V = V, formula0 = NULL,
+  logLik = fitGLS(formula = formula, data = data, V = V, formula0 = NULL,
                   save.xx = FALSE, save.invchol = FALSE, logLik.only = TRUE, no.F = TRUE,
-                  nugget = nug))
+                  nugget = nug)
   return(-logLik)
 }
 
