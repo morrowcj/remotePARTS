f515ea4652839b20215ebbbda405c7ac35c0b0b5
Clay Morrow <morrowcj@outlook.com>
2023-08-30 13:42:35 -0500

    fixed path in build pipeline

--- build_pipeline/full_build_pipeline.R
+++ build_pipeline/full_build_pipeline.R
@@ -1,4 +1,4 @@
 source("build_pipeline/document_package.R")
 devtools::check()
-source("build.R")
-source("render_vignette.R")
+source("build_pipeline/build.R")
+source("build_pipeline/render_vignette.R")
