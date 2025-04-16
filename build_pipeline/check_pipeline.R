c7165fe77c5771e3a786d6102dff39d5f8276240
morrowcj <25436787+morrowcj@users.noreply.github.com>
2025-04-15 22:34:34 -0500

    update build pipeline

--- build_pipeline/check_pipeline.R
+++
@@ -1,2 +0,0 @@
-source("build_pipeline/document_package.R")
-devtools::check()
--- build_pipeline/full_build_pipeline.R
+++ build_pipeline/full_build_pipeline.R
@@ -1,4 +1,5 @@
 source("build_pipeline/document_package.R")
-devtools::check(build_args = c("--resave-data"))
+source("build_pipeline/rcmd_check.R")
 source("build_pipeline/build.R")
+source("build_pipeline/install.R")
 source("build_pipeline/render_vignette.R")
