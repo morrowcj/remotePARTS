6e4ccdc43877e1e1da60d5662e9e3d6ede86992f
morrowcj <25436787+morrowcj@users.noreply.github.com>
2025-04-15 22:27:09 -0500

    update check options

--- build_pipeline/full_build_pipeline.R
+++ build_pipeline/full_build_pipeline.R
@@ -1,4 +1,4 @@
 source("build_pipeline/document_package.R")
-devtools::check()
+devtools::check(build_args = c("--resave-data"))
 source("build_pipeline/build.R")
 source("build_pipeline/render_vignette.R")
